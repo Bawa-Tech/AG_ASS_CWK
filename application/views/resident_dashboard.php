d9d6d825e1aa25bb84a603863538e9706af2e9a8
Mubeen Ghauri <dev@Devs-MacBook-Pro.local>
2023-04-25 01:50:55 +0500

    add vote for resident

--- application/views/resident_dashboard.php
+++ application/views/resident_dashboard.php
@@ -1,16 +1,43 @@
 <div class="container">
 
+    <div class="row">
+        <div class="container">
+            <?php
+            if ($this->session->flashdata('error')) {
+                echo '<div class="alert alert-danger">' . $this->session->flashdata('error') . '</div>';
+            }
+            ?>
+
+        </div>
+    </div>
     <div style="display: flex; justify-content:center; margin-top: 100px; text-align: center;">
+
+
+
         <?php
-        $this->table->set_heading(['#', 'Product Name', 'description', 'Size', 'Type', 'Price Band', 'Company Name']);
+        $this->table->set_heading(['#', 'Product Name', 'description', 'Size', 'Type', 'Price Band', 'Price', 'Company Name']);
         $this->table->set_caption('Products');
         $this->table->set_template(array(
             'table_open' => '<table class="table table-striped table-hover">'
         ));
-        echo $this->table->generate($products);
 
+        foreach ($products as $p) {
+            $id = $p["id"];
+            $product_name = $p["product_name"];
+            $product_description = $p["product_description"];
+            $size = $p["size"];
+            $type = $p["type"];
+            $price_band = $p["price_band"];
+            $price = $p["price"];
+            $sme_name = $p["sme_name"];
+
+            $vote_button = "<a href='" . base_url("index.php/resident/vote/" . $id) . "'><span class='btn btn-primary' style='color: white; align-self: right;'>Add Product</span></a>";
+
+
+            $this->table->add_row($id, $product_name, $product_description, $size, $type, $price_band, $price, $sme_name, $vote_button);
+        }
+
+        echo $this->table->generate();
         ?>
-
-
     </div>
 </div>
--- application/views/resident_product.php
+++ application/views/resident_product.php
@@ -5,7 +5,7 @@
     </div>
     <div style="display: flex; justify-content:center; margin-top: 100px; text-align: center;">
         <?php
-        $this->table->set_heading('#', 'Product Name', 'description', 'Size', 'Type', 'Price Band', 'Company Name');
+        $this->table->set_heading('#', 'Product Name', 'description', 'Size', 'Type', 'Price Band', 'Price', 'Company Name');
         $this->table->set_caption('Products');
         $this->table->set_template(array(
             'table_open' => '<table class="table table-striped table-hover">'
